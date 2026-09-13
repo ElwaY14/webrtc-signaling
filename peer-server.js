872f248915ea8deb4cdf52189cd1e03efead5883
ElwaY14 <alihusn59@gmail.com>
2026-09-13 11:12:36 +0700

    Fix PeerJS Express server

--- peer-server.js
+++ peer-server.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const { PeerServer } = require('peer');
+const { ExpressPeerServer } = require('peer');
+
 const app = express();
 const PORT = process.env.PORT || 9000;
 
-app.get('/', (_req, res) => res.send('Ultah WebRTC signaling server is running ♡'));
+app.get('/', (_req, res) => {
+  res.send('Ultah WebRTC signaling server is running');
+});
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`HTTP server listening on ${PORT}`);
 });
 
-PeerServer({ path: '/peerjs', server, allow_discovery: true });
+const peerServer = ExpressPeerServer(server, {
+  path: '/',
+  proxied: true,
+  allow_discovery: true,
+  debug: true
+});
+
+app.use('/peerjs', peerServer);
+
 process.on('SIGTERM', () => server.close(() => process.exit(0)));
